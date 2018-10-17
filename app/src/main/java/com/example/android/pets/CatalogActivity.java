fbbb6350c665ed253dc5578b47381c20a28577ef
Vicente Lozano <vicentelozano@gmail.com>
2018-10-17 13:31:22 +0200

    feat: Make CatalogActivity use the PetProvider via getContentResolver instead of db

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -70,7 +70,7 @@ public class CatalogActivity extends AppCompatActivity {
      */
     private void displayDatabaseInfo() {
         // Create and/or open a database to read from it
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        //SQLiteDatabase db = mDbHelper.getReadableDatabase();
 
         // Define a projection that specifies which columns from the database
         // you will actually use after this query.
@@ -81,8 +81,9 @@ public class CatalogActivity extends AppCompatActivity {
                 PetEntry.COLUMN_PET_GENDER,
                 PetEntry.COLUMN_PET_WEIGHT };
 
+
         // Perform a query on the pets table
-        Cursor cursor = db.query(
+        /**Cursor cursor = db.query(
                 PetEntry.TABLE_NAME,   // The table to query
                 projection,            // The columns to return
                 null,                  // The columns for the WHERE clause
@@ -90,6 +91,13 @@ public class CatalogActivity extends AppCompatActivity {
                 null,                  // Don't group the rows
                 null,                  // Don't filter by row groups
                 null);                   // The sort order
+        */
+        Cursor cursor = getContentResolver().query(
+                PetEntry.CONTENT_URI,   // The table to query
+                projection,             // The columns to return
+                null,          // The columns for the WHERE clause
+                null,       // The values for the WHERE clause
+                null);        // The sort order
 
         TextView displayView = (TextView) findViewById(R.id.text_view_pet);
 
