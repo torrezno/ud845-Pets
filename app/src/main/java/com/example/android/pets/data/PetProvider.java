6ca68279dfd96ba1ab498e4255725e7e07465544
Vicente Lozano <vicentelozano@gmail.com>
2018-10-17 13:23:15 +0200

    feat: Add logic to query method in PetProvider

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -3,9 +3,11 @@ package com.example.android.pets.data;
 
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 /**
@@ -38,7 +40,26 @@ public class PetProvider extends ContentProvider {
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        return null;
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        Cursor cursor=null;
+
+        int match = sUriMatcher.match(uri);
+        switch(match){
+            case PETS:
+                cursor = database.query(PetContract.PetEntry.TABLE_NAME, projection,selection,selectionArgs,
+                        null,null,sortOrder);
+                break;
+            case PET_ID:
+                selection = PetContract.PetEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri))};
+                cursor = database.query(PetContract.PetEntry.TABLE_NAME, projection,selection,selectionArgs,
+                        null,null,sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     @Override
